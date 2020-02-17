8af826582ad08500b7c77e46f9c36ba1bb402fd7
Hiago Rangel <hiago23rangel@gmail.com>
2020-02-17 11:07:19 -0300

    rota dale

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended: false}));
 
-app.get('/', (req, res)=>{
+app.get('/dale', (req, res)=>{
     res.send('funcionando')
 })
 
